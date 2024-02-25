46b98e46f63f8a93cbe8548f0d3090275f155bfd
“Hasan” <hasankhan@gmail.com>
2024-02-25 12:15:57 -0500

    First Commit

---
+++ Github_Eclipse_1/src/main/java/JuneBatch/Github_Eclipse_1/App.java
@@ -0,0 +1,13 @@
+package JuneBatch.Github_Eclipse_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
